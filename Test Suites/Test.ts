c7350bf51a3c7973b2aa759b3a8bb76f3b2f43d7
Seraphin Bopda <seraphin.bopda@opencellsoft.com>
2025-08-25 12:11:00 +0200

    ajout du fichier Test dans TestSuites

---
+++ Test Suites/Test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a2ab4d54-870b-4bb4-9f43-68db40d27b46</testSuiteGuid>
+   <testCaseLink>
+      <guid>81436aac-936a-48a5-bb81-a139a8cfcf89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
